e553a7f6cba06ab04d9b176e2100aff995a186a7
mayconbalves <mayconmustaine@hotmail.com>
2019-01-06 17:11:53 -0200

    feat(config): create messages of error

--- service/src/config/data-base.js
+++ service/src/config/data-base.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.require = "O atibruto '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O '{VALUE}' é menor do que o limite min '{MIN}' "
+mongoose.Error.messages.Number.max = "O '{VALUE}' é maio do que o limite maxímo '{MAX}' "
+mongoose.Error.messages.String.enum = " '{VALUE}' não é válido para o atributo '{PATH}' "
